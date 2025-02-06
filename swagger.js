f153483c93abac1766b197d13a61483139eca428
ryanwalk01 <ryan.walk01@gmail.com>
2025-02-06 12:38:59 -0700

    Fixed swagger

--- swagger.js
+++ swagger.js
@@ -1,18 +1,20 @@
 const router = require('express').Router();
 const swaggerAutogen = require('swagger-autogen')();
 
+const host = process.env.HOST || 'localhost:8080';
+
 const doc = {
     info: {
         title: 'TOTK API',
         description: 'API to manage a database of "The Legend of Zelda: Tears of the Kingdom" Shrines and Skyview Towers'
     },
-    host: 'cse341-project-ryan.onrender.com',
-    schemes: ['https'],
+    host: host,
+    schemes: ['http'],
     basePath: '/'
 };
 
 const outputFile = './swagger.json';
-const routes = ['./routes/shrines.js', './routes/towers.js'];
+const routes = ['./routes/index.js'];
 
 swaggerAutogen(outputFile, routes, doc);
 
