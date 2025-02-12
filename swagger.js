82760387dc2feeb787af6a7726715136fe40990e
ryanwalk01 <ryan.walk01@gmail.com>
2025-02-12 16:28:39 -0700

    Fixed API formatting

--- swagger.js
+++ swagger.js
@@ -4,7 +4,7 @@ const swaggerAutogen = require('swagger-autogen')();
 const doc = {
     info: {
         title: 'TOTK API',
-        description: 'API to manage a database of "The Legend of Zelda: Tears of the Kingdom" Shrines and Skyview Towers'
+        description: 'API to manage a database of "The Legend of Zelda: Tears of the Kingdom" Shrines and Skyview Towers.'
     },
     host: 'cse341-project-ryan.onrender.com',
     // host: 'localhost:8080',
