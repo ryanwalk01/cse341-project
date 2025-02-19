220e629c96a25ee373693f964cf65b8c5fca0b97
ryanwalk01 <ryan.walk01@gmail.com>
2025-02-19 16:24:26 -0700

    Added Auth0/OAuth

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,11 @@
+const { requiresAuth } = require('express-openid-connect');
+
 const router = require('express').Router();
 
 router.use('/towers', require('./towers'));
 
 router.use('/shrines', require('./shrines'));
 
-router.use('/api-docs', require('./swagger'));
+router.use('/api-docs', requiresAuth(), require('./swagger'));
 
 module.exports = router;
